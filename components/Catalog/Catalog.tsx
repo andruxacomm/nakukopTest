02b712d76bd620055f806d09d90346e28a3231f2
andrej <andruxa.comm@gmail.com>
2021-05-05 02:30:20 +0300

    finally fixes

--- components/Catalog/Catalog.tsx
+++ components/Catalog/Catalog.tsx
@@ -7,25 +7,27 @@ import { CatalogProduct } from './CatalogProduct';
 export const Catalog: FC = observer(() => {
     const { productsStore } = useStore();
     const { groupedGoods } = productsStore;
-
     return (
         <SimpleGrid columns={2} spacing={5}>
-            {groupedGoods.map(group => (
-                <Box key={group.id} p={5} borderWidth="1px" borderRadius="lg">
-                    <Heading as="h2" size="4sm" marginBottom={3} isTruncated>
-                        {group.name}
-                    </Heading>
-                    <Divider marginBottom={3} />
-                    {group.goods.length > 0
-                        ? group.goods.map((product, index) => (
-                              <Box key={product.id}>
-                                  <CatalogProduct product={product} />
-                                  {index === group.goods.length - 1 ? <Divider marginBottom={3} /> : null}
-                              </Box>
-                          ))
-                        : null}
-                </Box>
-            ))}
+            {groupedGoods.map(group => {
+                const { id, name, goods } = group;
+                return (
+                    <Box key={id} p={5} borderWidth="1px" borderRadius="lg">
+                        <Heading as="h2" size="4sm" marginBottom={3} isTruncated>
+                            {name}
+                        </Heading>
+                        <Divider marginBottom={3} />
+                        {goods.length > 0
+                            ? goods.map((product, index) => (
+                                  <Box key={product.id}>
+                                      <CatalogProduct product={product} />
+                                      {index === goods.length - 1 ? <Divider marginBottom={3} /> : null}
+                                  </Box>
+                              ))
+                            : null}
+                    </Box>
+                );
+            })}
         </SimpleGrid>
     );
 });
